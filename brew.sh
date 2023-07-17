68e09dac96d226362e4d448fb2ac7cb1caa165d0
Dan Barrett <dan@danbarrett.com.au>
2023-07-17 14:15:09 +1000

    feat: add configs for Finder and git

--- brew.sh
+++ brew.sh
@@ -16,6 +16,11 @@ rustup completions bash > ~/.local/share/bash-completion/completions/rustup
 
 # macOS Changes.
 
+# Show all the bars! Well, except for the Tab bar, because I don't use Finder tabs.
+defaults write com.apple.Finder ShowPathbar 1
+defaults write com.apple.Finder ShowSidebar 1
+defaults write com.apple.Finder ShowStatusBar 1
+
 # Configure the default screenshot location and format.
 defaults write com.apple.sceencapture location ~/Downloads
 defaults write com.apple.sceencapture type jpeg
@@ -26,70 +31,15 @@ defaults write -g ApplePressAndHoldEnabled -bool true
 # Git Changes.
 git config --global init.defaultBranch main
 
-# Install command-line tools using Homebrew.
+git config --global core.ignorecase true
 
-# Make sure we’re using the latest Homebrew.
-brew update
+git config --global pull.rebase true
 
-# Upgrade any already-installed formulae.
-brew upgrade
-
-# Save Homebrew’s installed location.
-BREW_PREFIX=$(brew --prefix)
-
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
-
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
+git config --global rerere.enabled true
+git config --global rerere.autoupdate true
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
   echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
-
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
-# Install more recent versions of some macOS tools.
-brew install grep
-brew install nano
-brew install openssh
-brew install screen
-
-# Install other useful binaries.
-brew install brotli
-brew install imagemagick --with-webp
-brew install mas
-brew install nmap
-brew install rename
-brew install ssh-copy-id
-brew install youtube-dl
-brew install zopfli
-
-# JavaScript.
-brew install node
-brew install yarn
-
-# Web Servers.
-brew install httpd
-brew install nginx
-
-# PHP related.
-brew install php@7.4
-
-# Remove outdated versions from the cellar.
-brew cleanup
