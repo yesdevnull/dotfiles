89c1a470a80658affdf81e224b5a4075772406a0
Dan Barrett <dan@danbarrett.com.au>
2023-10-14 19:04:33 +1100

    fix: adjustments for Rust setup

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -3,7 +3,11 @@
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+source "$HOME/.cargo/env"
+
 source ~/.profile
+
+mkdir -p ~/.local/share/bash-completion/completions/
 
 # https://rust-lang.github.io/rustup/installation/index.html#enable-tab-completion-for-bash-fish-zsh-or-powershell
 rustup completions bash > ~/.local/share/bash-completion/completions/rustup
